a2f302f929c02935825cd34f95b4c3f40083be77
Wei Hao <weihao@uber.com>
2018-11-11 16:05:53 -0700

    [BRCAM-1000] a

--- colmap_script/l2_fit.py
+++ colmap_script/l2_fit.py
@@ -42,9 +42,9 @@ for a in range(zone):
     ax1 = fig.add_subplot(c, r, a+1)
     ax1.scatter(da[:, 0], da[:, a+1],  color='black', s=5)
     ax1.plot(da[:, 0], pre1, color='blue', linewidth=2)
-    #ymin = np.min(da[:, a+1])
-    #ymax = np.max(da[:, a+1])
-    #ax1.set_ylim([ymin, ymax])
+    ymin = np.min(da[length:, a+1])
+    ymax = np.max(da[length:, a+1])
+    ax1.set_ylim([ymin, ymax*2-ymin])
 
     #
     # slope1, intercept1, r_value, p_value, std_err = stats.linregress(da[:, 0], da[:, 2])
