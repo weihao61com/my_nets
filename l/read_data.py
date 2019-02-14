2e1a2635ddca57261fa0464e1063273108629a25
wei hao <weihao61@gmail.com>
2019-02-14 07:42:44 -0700

    2-14'

--- l/read_data.py
+++ l/read_data.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+from sortedcontainers import SortedDict
 
 
 class Truth:
@@ -26,7 +27,7 @@ class Data:
         mtx = np.array(map(float, newstr)).reshape((self.numMeasurements, 3))
         self.measurements = {}
         for a in range(self.numMeasurements):
-            self.measurements[mtx[a, 0]] = list(mtx[a, 1:])
+            self.measurements[int(mtx[a, 0])] = list(mtx[a, 1:])
 
     def add_sensor(self, s):
         if s.id in self.measurements:
@@ -50,33 +51,28 @@ class TruthData:
 
 class Measurements:
 
-    def __init__(self, filename):
-        self.header = None
-        self.data = {}
-        self.test = {}
-        with open(filename, 'r') as fp:
-            csv_reader = csv.reader(fp, delimiter=',')
-            for row in csv_reader:
-                #print row
-                if self.header is None:
-                    self.header = row
-                    print row
-                else:
-                    d = Data(row)
-                    if d.missing:
-                        self.test[d.id] = d
-                    else:
-                        self.data[d.id] = d
-                if len(self.data)>1e6:
-                    break
-        print 'total data', len(self.data), len(self.test)
+    def __init__(self, rows, a, sensors, verbose=True):
+        self.data = SortedDict()
 
-    def setup_sensor(self, sensors):
-        for s in sensors:
-            for d in self.data:
-                self.data[d].add_sensor(s)
-            for d in self.test:
-                self.test[d].add_sensor(s)
+        for row in rows:
+            d = Data(row)
+            self.data[d.timeAtServer] = d
+
+        print 'total data', a, len(self.data)
+
+        if verbose:
+            filename = '/home/weihao/tmp/m_{}.csv'.format(a)
+            with open(filename, 'w') as fp:
+                for t in self.data:
+                    dd = self.data[t]
+                    for m in dd.measurements:
+                        mm = dd.measurements[m]
+                        ss = sensors.data[m]
+                        lla = ss.lla
+                        fp.write('{},{},{},{},{},{},{},{},{},{},{},{}\n'.
+                                 format(t, dd.id, dd.lat, dd.lon, dd.bAlt, dd.gAlt,
+                                        m, mm[0], mm[1], lla[0], lla[1], lla[2]))
+
 
 class Sensor:
     def __init__(self, rows):
@@ -88,7 +84,7 @@ class Sensor:
 class Sensors:
     def __init__(self, filename):
         self.header = None
-        self.data = []
+        self.data = {}
         with open(filename, 'r') as fp:
             csv_reader = csv.reader(fp, delimiter=',')
             for row in csv_reader:
@@ -96,23 +92,53 @@ class Sensors:
                     print row
                     self.header = row
                 else:
-                    self.data.append(Sensor(row))
+                    s = Sensor(row)
+                    self.data[s.id] = s
         print 'Total Sensor', len(self.data)
 
-def validate(data):
-    for d in data:
-        if len(d.measurements)!=6:
-            raise Exception('hah')
+
+def read_aircraft(filename):
+    aircrafts = SortedDict()
+    missing = 0
+    header = None
+
+    with open(filename, 'r') as fp:
+        csv_reader = csv.reader(fp, delimiter=',')
+        for row in csv_reader:
+            if header is None:
+                header = row
+                print row
+            else:
+                d = Data(row)
+                if d.missing:
+                    missing += 1
+                else:
+                    if not d.aircraft in aircrafts:
+                        aircrafts[d.aircraft] = []
+                    aircrafts[d.aircraft].append(row)
+            #if len(aircrafts)>10:
+            #    break
+
+    print 'total aircraft', len(aircrafts), missing
+    return aircrafts
+
 
 if __name__ == '__main__':
-    filename = '/home/weihao/PY/al/training_1_category_4/training_1_category_4.csv'
-    truthfile = '/home/weihao/PY/al/training_1_category_4_result/training_1_category_4_result.csv'
-    sensorfile = '/home/weihao/PY/al/training_1_category_4/sensors.csv'
+    data_location = '/home/weihao/PY/al'
+    data_set = 'training_1_category_4'
+    filename = '{0}/{1}/{1}.csv'.format(data_location, data_set)
+    truthfile = '{0}/{1}_result/{1}_result.csv'.format(data_location, data_set)
+    sensorfile = '{0}/{1}/sensors.csv'.format(data_location, data_set)
 
     sensors = Sensors(sensorfile)
     truth = TruthData(truthfile)
-    data = Measurements(filename)
+    aircrafts = read_aircraft(filename)
 
-    data.setup_sensor(sensors.data)
+    measure = {}
+    for a in aircrafts:
+        measure[a] = Measurements(aircrafts[a], a, sensors)
+        if len(measure)>4:
+            break
 
-    validate(data)
+    #data.setup_sensor(sensors.data)
+
